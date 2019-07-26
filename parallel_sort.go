0a36ad500d3f6040705f375bccd72d8d2fe5a34a
dawidjk <dawidjk2@illinois.edu>
2019-07-25 21:10:17 -0400

    Parallel load

--- parallel_sort.go
+++ parallel_sort.go
@@ -1,8 +1,10 @@
 package terasort
 
 import (
+	"encoding/binary"
 	"log"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -10,6 +12,7 @@ func main() {
 	totalFileSize := oneGig * 8
 	totalMemoryGb := 4
 	readSize := oneGig
+	bytesInInt64 := 8
 
 	fileName := "/Users/dave07747/Development/Terabyte-Sort/eightGB.bin"
 
@@ -21,10 +24,28 @@ func main() {
 	}
 
 	file, err := os.OpenFile(fileName, os.O_RDONLY, os.ModeAppend)
+	readBytes := readNextSector(file, readSize)
 
+	uint64count := readSize / bytesInInt64
+	toBeSorted := make([]uint64, uint64count)
+
+	var wg sync.WaitGroup
+	wg.Add(uint64count)
+
+	for i := 0; i < uint64count; i++ {
+		go func(i int) {
+			lowerBoundary := 8 * i
+			upperBoundary := 8 * (i + 1)
+			toBeSorted[i] = bytesToInt64(readBytes[lowerBoundary:upperBoundary])
+		}(i)
+	}
 }
 
-func readNextSector(file *os.File, number length) {
+func bytesToInt64(data []byte) uint64 {
+	return binary.BigEndian.Uint64(data)
+}
+
+func readNextSector(file *os.File, number int) []byte {
 	bytes := make([]byte, number)
 	_, err := file.Read(bytes)
 
